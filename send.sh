637a46edb110a4878374ee7730a6bfc99650f972
João Fernandes <joaofnds@joaofnds.com>
2022-06-29 16:19:22 -0300

    remove \n scape

--- send.sh
+++ send.sh
@@ -4,5 +4,5 @@ curl -i\
   -H "Accept: application/json" \
   -H "Content-Type:application/json" \
   -X POST \
-  --data "{\"content\": \"$(shuf -n 1 tips.txt | sed 's/\\n/\n/g')\"}" \
+  --data "{\"content\": \"$(shuf -n 1 tips.txt)\"}" \
   "$WEBHOOK_URL"
