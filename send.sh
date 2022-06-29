17f70fa9d9aa250b20244bcf1066b8573ed09dcf
João Fernandes <joaofnds@joaofnds.com>
2022-06-29 16:18:11 -0300

    update tips

--- send.sh
+++ send.sh
@@ -4,5 +4,5 @@ curl -i\
   -H "Accept: application/json" \
   -H "Content-Type:application/json" \
   -X POST \
-  --data "{\"content\": \"$(shuf -n 1 tips.txt)\"}" \
+  --data "{\"content\": \"$(shuf -n 1 tips.txt | sed 's/\\n/\n/g')\"}" \
   "$WEBHOOK_URL"
